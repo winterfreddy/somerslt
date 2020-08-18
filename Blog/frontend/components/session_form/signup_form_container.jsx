8d9979504a8ebd0440b414a38357ed8142fafc4b
Winfred Huang <huang.winfred21@gmail.com>
2020-08-18 15:44:09 -0700

    styling changes

--- Blog/frontend/components/session_form/signup_form_container.jsx
+++ Blog/frontend/components/session_form/signup_form_container.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import SessionForm from './session_form'
 import { signup } from '../../actions/session_actions';
+import { Link } from 'react-router-dom';
 
 const mapStateToProps = (state) => ({
     errors: state.errors.session,
-    formType: 'signup'
+    formType: 'signup',
+    navLink: <Link to="/login">log in instead</Link>
 })
 
 const mapDispatchToProps = (dispatch) => ({
