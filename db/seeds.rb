9b77ddb07b81f0ef21ed1ef3f091735a903d2983
Winfred Huang <huang.winfred21@gmail.com>
2020-08-18 17:31:51 -0700

    Added demo user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+# for demo user login
+User.create!(
+  username: 'something',
+  email: 'something@something.com',
+  password: 'something'
+)
