36ece891052e6361143327bdf340998ede58faab
Winfred Huang <huang.winfred21@gmail.com>
2020-08-20 11:02:02 -0700

    association fix

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -14,7 +14,7 @@ class Blog < ApplicationRecord
     validates :body, presence: true
     validates :author_id, presence: true
 
-    belongs_to :author,
+    belongs_to :user,
         primary_key: :id,
         foreign_key: :author_id,
         class_name: :User
