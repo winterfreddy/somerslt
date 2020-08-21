7786c7cd27086645c9c55ba729e112fd651b9ebe
Winfred Huang <huang.winfred21@gmail.com>
2020-08-21 11:32:36 -0700

    Styling changes to nav bar

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom';
 const Greeting = (props) => {
     const newSession = () => {
         return(
-            <nav className="login-signup">
+            <nav className="header-nav">
                 <Link to="/" className="header-logo">S</Link>
-                <Link to="/login" className="login-link">Log in</Link>
-                <Link to="/signup" className="signup-link">Sign up</Link>
+                <div className="header-pane">
+                    <Link to="/login" className="login-link">Log in</Link>
+                    <Link to="/signup" className="signup-link">Sign up</Link>
+                </div>
             </nav>
         );
     };
     const currSession = () => {
         return(
-            <hgroup className="header-group">
+            <nav className="dashboard-nav">
+                {/* <div className="transparent-header"></div> */}
                 <Link to="/dashboard" className="header-logo">S</Link>
                 {/* <h2 className="header-name">{props.currentUser.username}</h2> */}
-                <button className="header-button" onClick={props.logout}>Log Out</button>
-            </hgroup>
+                <button className="logout-button" onClick={props.logout}>Log Out</button>
+            </nav>
         );
     };
 
