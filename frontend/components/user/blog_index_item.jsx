40057cafc45267c14c9851cb3f9beb3bb95edb77
Winfred Huang <huang.winfred21@gmail.com>
2020-08-23 13:42:52 -0700

    Updated each post to include source

--- frontend/components/user/blog_index_item.jsx
+++ frontend/components/user/blog_index_item.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function figureOwner(props) {
-    let blogId = props.blog.authorId;
+    let blogId = props.blog.author_id;
     let blogOwner;
     props.allUsers.forEach( user => {
+        // console.log(user);
         if(user.id === blogId) {
+            // console.log("checks off");
             blogOwner = user.username;
+        }
+        else {
+            blogOwner = "user-deactivated";
         }
     });
     return(
@@ -24,11 +29,17 @@ const BlogIndexItem = (props) => {
             <h1 className="avatar"></h1>
             <div className="blog-info">
                 <div className="blog-title">
-                    <h1>{props.blog.title}</h1>
-                    {/* {figureOwner(props)} */}
+                    {figureOwner(props)}
                     <Link to={`/blogs/${props.blogId}/edit`} className="edit-link">Edit</Link>
                 </div>
                 <p className="blog-body">{props.blog.body}</p>
+                <div className="blog-description">
+                    <p>{props.blog.title}</p>
+                    <div className="blog-source">
+                        <p>Source:&nbsp;</p>
+                        {figureOwner(props)}
+                    </div>
+                </div>
                 <div className="blog-footer">
                     <label className="blog-notes">X notes</label>
                     <label className="blog-like">Like</label>
