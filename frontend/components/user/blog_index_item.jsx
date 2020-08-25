eac4c0e65030b1b20807ba10e622b8a00d8d3832
Winfred Huang <huang.winfred21@gmail.com>
2020-08-25 15:01:01 -0700

    render correct edit links per blog post

--- frontend/components/user/blog_index_item.jsx
+++ frontend/components/user/blog_index_item.jsx
@@ -18,9 +18,32 @@ function ownerActions(props) {
     if(props.currentUser.id === props.blog.author_id) {
         return(
             <div>
-                <Link to={`/blogs/${props.blog.id}/edit_text`} className="edit-link"><i class="fas fa-edit"></i></Link>
+                {renderLinks(props.blog.media_type, props.blog.id)}
                 <button className="delete-button" onClick={() => props.deleteBlog(props.blog)}><i class="fas fa-trash-alt"></i></button>
             </div>
+        )
+    }
+}
+
+function renderLinks(media_type, blogId) {
+    if(media_type === 'photo') {
+        return (
+            <Link to={`/blogs/${blogId}/edit_photo`} className="edit-link"><i class="fas fa-edit"></i></Link>
+        )
+    }
+    if(media_type === 'quote') {
+        return (
+            <Link to={`/blogs/${blogId}/edit_quote`} className="edit-link"><i class="fas fa-edit"></i></Link>
+        )
+    }
+    if(media_type === 'url') {
+        return (
+            <Link to={`/blogs/${blogId}/edit_url`} className="edit-link"><i class="fas fa-edit"></i></Link>
+        )
+    }
+    else {
+        return (
+            <Link to={`/blogs/${blogId}/edit_text`} className="edit-link"><i class="fas fa-edit"></i></Link>
         )
     }
 }
@@ -56,7 +79,6 @@ function renderBody(props) {
         )
     }
     if(media_type === 'url') {
-        console.log("this is a quote");
         return(
             <div className="url-description">
                 <a href={props.blog.title} className="url-title-link">{props.blog.title}</a>
