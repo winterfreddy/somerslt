8f32b3b325c0f68cc6a057a358c1bcac5bfd2d0d
Winfred Huang <huang.winfred21@gmail.com>
2020-08-26 13:57:29 -0700

    Updated syntax to remove "class" warnings

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -20,11 +20,11 @@ const Greeting = (props) => {
                 {/* <div className="transparent-header"></div> */}
                 <Link to="/dashboard" className="header-logo">S</Link>
                 <div className="dropdown">
-                    <button onClick={() => dropdown()} className="dropdown-button" type="button"><i class="fas fa-user"></i></button>
+                    <button onClick={() => dropdown()} className="dropdown-button" type="button"><i className="fas fa-user"></i></button>
                     <div id="myDropdown" className="dropdown-content">
                         <div className="dropdown-header">
                             <label className="account-label">Account</label>
-                            <button className="logout-button" onClick={props.logout}>Log out&nbsp;&nbsp;<i class="fas fa-sign-out-alt"></i></button>
+                            <button className="logout-button" onClick={props.logout}>Log out&nbsp;&nbsp;<i className="fas fa-sign-out-alt"></i></button>
                         </div>
                         <div className="dropdown-like">
                             <a href="#"><i class="fas fa-heart"></i>&nbsp;Likes</a>
