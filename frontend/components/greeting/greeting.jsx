9b736628c2445113665ea04d4ce7ca62dc22dfb8
Winfred Huang <huang.winfred21@gmail.com>
2020-08-27 02:02:28 -0700

    Added personal links

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom';
 const Greeting = (props) => {
     const newSession = () => {
         return(
-            <Link to="/" className="header-logo">S</Link>
+            <div>
+                <Link to="/" className="header-logo">S</Link>
+                <div className="personal-links">
+                    <a href="https://github.com/winterfreddy/somerslt/wiki" className="github-link"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/winfred-huang/" className="linkedin-link"><i className="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
         );
     };
 
@@ -16,25 +22,32 @@ const Greeting = (props) => {
         }
 
         return(
-            <nav className="dashboard-nav">
-                {/* <div className="transparent-header"></div> */}
-                <Link to="/dashboard" className="header-logo">S</Link>
-                <div className="dropdown">
-                    <button onClick={() => dropdown()} className="dropdown-button" type="button"><i className="fas fa-user"></i></button>
-                    <div id="myDropdown" className="dropdown-content">
-                        <div className="dropdown-header">
-                            <label className="account-label">Account</label>
-                            <button className="logout-button" onClick={props.logout}>Log out&nbsp;&nbsp;<i className="fas fa-sign-out-alt"></i></button>
-                        </div>
-                        {/* <div className="dropdown-like">
-                            <a href="#"><i className="fas fa-heart"></i>&nbsp;Likes</a>
-                        </div> */}
-                        <div className="dropdown-follow">
-                            <a href="#"><i className="fas fa-user-plus"></i>&nbsp;Following</a>
+            <div>
+                <nav className="dashboard-nav">
+                    {/* <div className="transparent-header"></div> */}
+                    <Link to="/dashboard" className="header-logo">S</Link>
+                    <div className="dropdown">
+                        <button onClick={() => dropdown()} className="dropdown-button" type="button"><i className="fas fa-user"></i></button>
+                        <div id="myDropdown" className="dropdown-content">
+                            <div className="dropdown-header">
+                                <label className="account-label">Account</label>
+                                <button className="logout-button" onClick={props.logout}>Log out&nbsp;&nbsp;<i className="fas fa-sign-out-alt"></i></button>
+                            </div>
+                            {/* <div className="dropdown-like">
+                                <a href="#"><i className="fas fa-heart"></i>&nbsp;Likes</a>
+                            </div> */}
+                            <div className="dropdown-follow">
+                                <a href="#"><i className="fas fa-user-plus"></i>&nbsp;Following</a>
+                            </div>
                         </div>
                     </div>
+                </nav>
+                <div className="personal-links">
+                    <a href="https://github.com/winterfreddy/somerslt/wiki" className="github-link"><i className="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/winfred-huang/" className="linkedin-link"><i className="fab fa-linkedin-in"></i></a>
                 </div>
-            </nav>
+
+            </div>
         );
     };
 
