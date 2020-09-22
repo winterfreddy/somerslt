a95dd0b22cdb819b5a0351473b82492cf1e93d9c
Winfred Huang <huang.winfred21@gmail.com>
2020-09-21 22:19:50 -0700

    Updated follows controller and jbuilder

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -2,15 +2,26 @@ class Api::FollowsController < ApplicationController
     before_action :require_user, only: [:create, :destroy]
 
     def index
-
+        @active_relationships = current_user.active_relationships
+        @passive_relationships = current_user.passive_relationships
     end
 
     def create
-
+        @followee_user = User.find(params[:follow][:followee_id])
+        @relationship = current_user.active_relationships.new(followee_id: @followee_user.id)
+        if @relationship.save!
+            flash[:message] = "Follow successful"
+        else
+            flash[:message] = "Follow unsuccessful"
+        end
     end
 
     def destroy
-
+        @relationship = Follow.find_by(id: params[:id])
+        if @relationship.follower_user == current_user
+            @relationship.destroy
+            flash[:message] = "Unfollowed"
+        end
     end
 
     private
--- app/views/api/users/index.json.jbuilder
+++ app/views/api/users/index.json.jbuilder
@@ -1,5 +1,8 @@
 @users.each do |user|
     json.set! user.id do
         json.partial! 'user', user: user
+        json.followIds user.active_relationships do |relationship|
+            json.extract! relationship, :id, :followee_id
+        end
     end
 end
